9760c2e7b6323cb8ff9aa1fa8be489b62991cbaa
lzzzcc <2839202038@qq.com>
2026-07-10 17:10:40 +0800

    Add GitHub Actions deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,16 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://zxxhbiii.github.io",
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
